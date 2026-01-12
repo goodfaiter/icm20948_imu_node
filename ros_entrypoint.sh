e7b46b1c97a151c6ac7365e7aeb35a7ebcfe5419
Valentin Yuryev <goodfaiter@gmail.com>
2026-01-12 17:01:05 +0100

    more robust entrypoint

--- ros_entrypoint.sh
+++ ros_entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 id -u ros &>/dev/null || adduser --quiet --disabled-password --gecos '' --uid ${UID:=1000} ros
 
 if ! getent group i2c > /dev/null; then
-    groupadd -g 988 i2c
+    groupadd -g ${I2C_GROUP_ID:=988} i2c
     echo "Created i2c group"
 fi
 # groupadd -g ${I2C_GROUP_ID:=988} i2c
